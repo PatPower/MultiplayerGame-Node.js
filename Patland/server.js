47f3f4727495c3d06f9f06fff08cf24bc298b36a
PatPower <patent.li@mail.utoronto.ca>
2019-12-28 03:57:10 -0500

    Added tile based movement

--- Patland/server.js
+++ Patland/server.js
@@ -5,7 +5,7 @@ var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 
-const port = 3000;
+const port = 80;
 
 app.set('port', port);
 
@@ -20,41 +20,60 @@ app.get('/josh', (req, res) => {
     res.render('index', {name: 'Josh'})
 });
 
-server.listen(3000, function(){
-    console.log('listening on *:3000');
+server.listen(port, function(){
+    console.log('listening on ' + port);
 });
 
-var players = {}
-
+var players = {};
+var moveLog = {};
+var MAXSPEED = 30;
 // Add the WebSocket handlers
 io.on('connection', function(socket) {
-    socket.on('new player', function() {
+    socket.on('new player', function(pname) {
         players[socket.id] = {
-            x: 300,
-            y: 300
+            id: socket.id,
+            i: 10,
+            j: 7,
+            name: pname,
+            color: 'red'
         }
+        io.to(socket.id).emit('setup', players, players[socket.id]);
+        io.sockets.emit('playerMove', {}, players[socket.id]);
+        moveLog[socket.id] = [(new Date).getTime()];
+        console.log(pname);
     });
+
+    socket.on('disconnect', function() {
+        io.sockets.emit('playerRemove', players[socket.id]);
+        delete players[socket.id];
+    });
+
     socket.on('movement', function(data) {
+        var currMoveLog = moveLog[socket.id];
+        console.log((currMoveLog[moveLog[socket.id].length-1] - currMoveLog[0]) + '\n' +currMoveLog.length)
+        if (currMoveLog[moveLog[socket.id].length-1] - currMoveLog[0] <= 2000 && currMoveLog.length >= MAXSPEED) {console.log("TOO FAST!"); return;}
         var player = players[socket.id] || {};
+        var oldPlayer = JSON.parse(JSON.stringify(player));
         if (data.left) {
-            player.x -= 5;
+            player.i -= 1;
         }
         if (data.right) {
-            player.x += 5;
+            player.i += 1;
         }
         if (data.up) {
-            player.y -= 5;
+            player.j -= 1;
         }
         if (data.down) {
-            player.y += 5;
+            player.j += 1;
         }
+        if (moveLog[socket.id].length >= MAXSPEED) {
+            moveLog[socket.id].shift();
+            moveLog[socket.id].push((new Date).getTime())
+        } else {
+            moveLog[socket.id].push((new Date).getTime())
+        }
+        io.sockets.emit('playerMove', oldPlayer, player);
+        console.log("move")
     });
 });
 
-setInterval(function() {
-    io.sockets.emit('state', players);
-}, 1000/60);
-
-setInterval(function() {
-    io.sockets.emit('message', 'hi!');
-}, 1000);
--- Patland/static/game.js
+++ Patland/static/game.js
@@ -1,4 +1,7 @@
 var socket = io();
+var currPlayer = {}
+
+
 socket.on('message', function (data) {
     console.log(data);
 });
@@ -10,56 +13,115 @@ var movement = {
     right: false
 }
 
+var canMove = true; var firstKeyHold = true;
+var canShift = false;
 document.addEventListener('keydown', function (event) {
-    switch (event.keyCode) {
-        case 65: //A
-            movement.left = true;
-            break;
-        case 87: // W
-            movement.up = true;
-            break;
-        case 68: // D
-            movement.right = true;
-            break;
-        case 83: // S
-            movement.down = true;
-            break;
-    }
+    if (event.keyCode == 87) {movement.up = true;} //W 
+    if (event.keyCode == 68) {movement.right = true;} //D
+    if (event.keyCode == 83) {movement.down = true;} //S
+    if (event.keyCode == 65) {movement.left = true;} //A
 });
 
 document.addEventListener('keyup', function (event) {
-    switch (event.keyCode) {
-        case 65: // A
-            movement.left = false;
-            break;
-        case 87: // W
-            movement.up = false;
-            break;
-        case 68: // D
-            movement.right = false;
-            break;
-        case 83: // S
-            movement.down = false;
-            break;
-    }
+    canMove = true;
+    if (event.keyCode == 87) {movement.up = false;} //W 
+    if (event.keyCode == 68) {movement.right = false;} //D
+    if (event.keyCode == 83) {movement.down = false;} //S
+    if (event.keyCode == 65) {movement.left = false;} //A
+    timeoutCounter = 0;
+    firstKeyHold = true
 });
 
-socket.emit('new player');
+var timeoutCounter = 0;
+setInterval(function(){
+    if(timeoutCounter >= 1) {timeoutCounter--; return false;}
+    if (movement.up|| movement.right|| movement.down|| movement.left) {
+        if (firstKeyHold) {timeoutCounter = 8; firstKeyHold = false} else {timeoutCounter = 3;}
+        socket.emit('movement', movement);
+    }
+},40);
+
+socket.emit('new player', pname);
+
+/** 
 setInterval(function () {
     socket.emit('movement', movement);
 }, 1000 / 60);
+*/
 
-var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
-var context = canvas.getContext('2d');
-socket.on('state', function (players) {
-    context.clearRect(0, 0, 800, 600);
-    context.fillStyle = 'green';
-    for (var id in players) {
-        var player = players[id];
-        context.beginPath();
-        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-        context.fill();
+var bcanvas = document.getElementById('canvas');
+bcanvas.width = 840;
+bcanvas.height = 600;
+var boxSide = 40;
+var numRow = Math.floor(bcanvas.width/boxSide);
+var numCol = Math.floor(bcanvas.height/boxSide);
+console.log('row: ' + numRow + '\n' + "col: " + numCol)
+var bgcxt = bcanvas.getContext('2d');
+for (var i = 0; i < numRow; i++) {
+    for (var j = 0; j < numCol; j++) {
+        bgcxt.strokeRect(boxSide*i,boxSide*j,boxSide,boxSide);
     }
+}
+
+var pcanvas = document.getElementById('player');
+pcanvas.width = 840;
+pcanvas.height = 600;
+var pcxt = pcanvas.getContext('2d');
+
+var otherpcanvas = document.getElementById('otherPlayers');
+pcanvas.width = 840;
+pcanvas.height = 600;
+var opcxt = pcanvas.getContext('2d');
+
+// squaresObj[].i
+// squaresObj[].j
+// squaresObj[].color
+function projectSquares(squaresObj) {
+    for (var index in squaresObj) {
+        projectSquare(squaresObj[index]);
+    }
+}
+
+// squaresObj[].i
+// squaresObj[].j
+// squaresObj[].color
+function projectSquare(playerObj) {
+    if (playerObj.id != currPlayer.id) {
+        opcxt.fillStyle = playerObj.color
+        opcxt.fillRect(boxSide*playerObj.i,boxSide*playerObj.j,boxSide,boxSide);
+        opcxt.fillStyle = 'blue'
+        opcxt.font = "12px Arial";
+        opcxt.fillText(playerObj.name, boxSide*playerObj.i, boxSide*playerObj.j+10);
+    } else {
+        pcxt.fillStyle = 'cyan';
+        pcxt.fillRect(boxSide*playerObj.i,boxSide*playerObj.j,boxSide,boxSide);
+        pcxt.fillStyle = 'blue'
+        pcxt.font = "12px Arial";
+        pcxt.fillText(playerObj.name, boxSide*playerObj.i, boxSide*playerObj.j+10);
+    }
+}
+/**
+ * 
+ * @param {*} squareObj The player being removed from the screen
+ */
+function removeProjectedPlayer(squareObj) {
+    pcxt.fillStyle = "white";
+    pcxt.fillRect(boxSide*squareObj.i,boxSide*squareObj.j,boxSide,boxSide);
+}
+
+socket.on('playerMove', function (oldP, newP) {
+    removeProjectedPlayer(oldP)
+    projectSquare(newP);
 });
+
+socket.on('setup', function(playerList, currentPlayer) {
+    currPlayer = currentPlayer;
+    projectSquares(playerList);
+    projectSquare(currentPlayer);
+});
+
+socket.on('playerRemove', function(player) {
+    removeProjectedPlayer(player);
+    console.log("player removee")
+})
+
