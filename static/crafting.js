16aa2fca29871c130cdb4981cc88e860a52010ad
PatPower <patent.li@mail.utoronto.ca>
2022-04-06 03:27:54 -0400

    Fixed small crafting issue on firefox

--- static/crafting.js
+++ static/crafting.js
@@ -1,12 +1,12 @@
 craftcanvas = document.getElementById("crafting");
 craftingArea = document.getElementById("craftingArea");
-var toolInvId = -1, craftItem1Id = -1, craftItem2Id = -1, craftItem3Id = -1;
-var toolInvPos = -1, craftItem1Pos = -1, craftItem2Pos = -1, craftItem3Pos = -1;
+var craftingItemIdList = [-1, -1, -1, -1];
+var craftingItemPosList = [-1, -1, -1, -1];
 
 function setupCraftingArea() {
     craftcanvas.width = CRAFTINGWIDTH;
     craftcanvas.height = CRAFTINGHEIGHT;
-    var cxt = craftcanvas.getContext('2d');
+    var cxt = craftcanvas.getContext("2d");
     cxt.fillStyle = "rgb(208, 146, 15)";
     cxt.fillRect(0, 0, CRAFTINGWIDTH, CRAFTINGHEIGHT);
 
@@ -16,35 +16,40 @@ function setupCraftingArea() {
     cxt.font = "bold 16px Almendra SC";
     cxt.fillText("Tool", CRAFTINGWIDTH / 2 - 15.5, 45);
 
-    var img = document.createElement('img');
+    var img = document.createElement("img");
     img.setAttribute("class", "craftitem");
-    img.setAttribute("id", "craftToolItem");
+    img.setAttribute("id", "craftItem0");
     img.src = getItemIcon(-1);
     craftingArea.append(img);
-    $("#craftToolItem").css({
+    $("#craftItem0").css({
         top: 51,
         left: CRAFTINGWIDTH / 2 - 20.5,
-    })
-    preventDragging("#craftToolItem");
-    makeCraftItemDroppable("#craftToolItem", 0);
+    });
+    preventDragging("#craftItem");
+    makeCraftItemDroppable("#craftItem0", 0);
 
     // Crafting boxes
-    for (i = 0; i < 3; i++) {
-        cxt.strokeRect(10 + i * (INVBOXSIDE + 10), 115, INVBOXSIDE, INVBOXSIDE);
+    for (i = 1; i <= 3; i++) {
+        cxt.strokeRect(
+            10 + (i - 1) * (INVBOXSIDE + 10),
+            115,
+            INVBOXSIDE,
+            INVBOXSIDE
+        );
         cxt.fillStyle = "black";
         cxt.font = "bold 16px Almendra SC";
-        cxt.fillText("Item " + `${i + 1}`, 10 + i * (INVBOXSIDE + 10), 108);
-        var img = document.createElement('img');
+        cxt.fillText("Item " + `${i}`, 10 + (i - 1) * (INVBOXSIDE + 10), 108);
+        var img = document.createElement("img");
         img.setAttribute("class", "craftitem");
         img.setAttribute("id", "craftItem" + i);
         img.src = getItemIcon(-1);
         craftingArea.append(img);
         $("#craftItem" + i).css({
             top: 116,
-            left: 10 + i * (INVBOXSIDE + 10),
-        })
+            left: 10 + (i - 1) * (INVBOXSIDE + 10),
+        });
         preventDragging("#craftItem" + i);
-        makeCraftItemDroppable("#craftItem" + i, i + 1);
+        makeCraftItemDroppable("#craftItem" + i, i);
     }
     craftTitle(cxt);
     return cxt;
@@ -55,14 +60,14 @@ function craftTitle(cxt) {
     cxt.fillRect(0, 0, INVWIDTH, 20);
     cxt.fillStyle = "black";
     cxt.strokeRect(0, 0, INVWIDTH, 20);
-    cxt.fillStyle = 'black';
+    cxt.fillStyle = "black";
     cxt.font = "bold 16px Almendra SC";
     cxt.fillText("Crafting", INVWIDTH / 2 - 30, 14);
 }
 
 /**
- * 
- * @param {*} id 
+ *
+ * @param {*} id
  * @param {*} craftItemPos  0: tool , 1: item 1, 2: item 2, 3: item 3
  */
 function makeCraftItemDroppable(id, craftItemPos) {
@@ -70,17 +75,19 @@ function makeCraftItemDroppable(id, craftItemPos) {
         disabled: false,
         drop: function (event, ui) {
             // The item being used
-            var draggedItemId = ui.draggable.attr('id');
+            var draggedItemId = ui.draggable.attr("id");
+
             // Show the item is being used for crafting
-            console.log("#" + draggedItemId)
 
             // Gets the position of the inv item being used
-            var itemPos = parseInt(draggedItemId.slice(4)) - 1
+            var itemPos = parseInt(draggedItemId.slice(4));
+            console.log(itemPos);
 
-            var invX = itemPos % 4;
-            var invY = Math.floor(itemPos / 4);
+            var invX = (itemPos - 1) % 4;
+            var invY = Math.floor((itemPos - 1) / 4);
 
-            var img = document.createElement('img');
+            // Adds black tint over item being used
+            var img = document.createElement("img");
             img.setAttribute("class", "itemOverlay");
             img.setAttribute("id", "itemOverlay" + craftItemPos);
             img.src = getItemIcon(-2);
@@ -89,38 +96,30 @@ function makeCraftItemDroppable(id, craftItemPos) {
             $("#itemOverlay" + craftItemPos).css({
                 top: invY * INVBOXSIDE,
                 left: invX * INVBOXSIDE,
-            })
+            });
 
             // Disable dragging/dropping for the used item
             preventDragging("#item" + draggedItemId.slice(4));
-            $('#item' + draggedItemId.slice(4)).droppable('disable');
+            $("#item" + draggedItemId.slice(4)).droppable("disable");
 
             // Disable dragging/dropping for overlay
             preventDragging("#itemOverlay" + craftItemPos);
 
             // Set the src to of the crafting box to the dragged from item
-            $(this).attr('src', ui.draggable.attr('src'))
+            $(this).attr("src", ui.draggable.attr("src"));
 
-            switch (craftItemPos) {
-                case 0:
-                    toolInvId = currPlayer.inventory[itemPos];
-                    toolInvPos = itemPos;
-                    break;
-                case 1:
-                    craftItem1Id = currPlayer.inventory[itemPos];
-                    craftItem1Pos = itemPos;
-                    break;
-                case 2:
-                    craftItem2Id = currPlayer.inventory[itemPos];
-                    craftItem2Pos = itemPos;
-                    break;
-                case 3:
-                    craftItem3Id = currPlayer.inventory[itemPos];
-                    craftItem3Pos = itemPos;
-                    break;
-            }
-
-        }
+            craftingItemIdList[craftItemPos] =
+                currPlayer.inventory[itemPos - 1].id;
+            craftingItemPosList[craftItemPos] = itemPos;
+        },
     });
 }
 
+// Removes item from crafting area and puts it back in the inventory
+function RemoveCraftingItem(itemPos) {
+    $("#itemOverlay" + itemPos).remove();
+    $("#craftItem" + itemPos).attr("src", getItemIcon(-1));
+    // Re-enable dragging/dropping for the used item
+    enableDragging("#item" + craftingItemPosList[itemPos]);
+    makeDroppable("#item" + craftingItemPosList[itemPos]);
+}
--- static/customContextMenu.js
+++ static/customContextMenu.js
@@ -3,18 +3,18 @@ var menuVisible = false;
 $(function () {
     // Map
     $.contextMenu({
-        selector: '#overlay, #click',
+        selector: "#overlay, #click",
         autoHide: true,
         hideOnSecondTrigger: true,
         position: function (opt, x, y) {
             opt.$menu.css({ top: y, left: x - 30 });
         },
         build: function ($triggerElement, e) {
-            $('#tooltip').hide();
+            $("#tooltip").hide();
 
-            var x = mousePos.x - $('#overlay').offset().left - 4;
-            var y = mousePos.y - $('#overlay').offset().top - 4;
-            return findMenuObj(x, y)
+            var x = mousePos.x - $("#overlay").offset().left - 4;
+            var y = mousePos.y - $("#overlay").offset().top - 4;
+            return findMenuObj(x, y);
         },
         events: {
             show: function (options) {
@@ -22,21 +22,23 @@ $(function () {
             },
             hide: function (options) {
                 menuVisible = false;
-            }
+            },
         },
-        zIndex: 4
+        zIndex: 4,
     });
 
     // Inventory
     $.contextMenu({
-        selector: '.item',
+        selector: ".item",
         autoHide: true,
         hideOnSecondTrigger: true,
         position: function (opt, x, y) {
             opt.$menu.css({ top: y, left: x - 30 });
         },
         build: function ($triggerElement, e) {
-            return findMenuObjInv(parseInt($triggerElement.attr("id").slice(4)) - 1);
+            return findMenuObjInv(
+                parseInt($triggerElement.attr("id").slice(4)) - 1
+            );
         },
         events: {
             show: function (options) {
@@ -44,66 +46,95 @@ $(function () {
             },
             hide: function (options) {
                 menuVisible = false;
-            }
+            },
         },
-        zIndex: 4
+        zIndex: 4,
     });
 
-
+    // Crafting
+    $.contextMenu({
+        selector: ".craftitem",
+        autoHide: true,
+        hideOnSecondTrigger: true,
+        position: function (opt, x, y) {
+            opt.$menu.css({ top: y, left: x - 30 });
+        },
+        build: function ($triggerElement, e) {
+            var craftVal = $triggerElement.attr("id").slice(9);
+            if (craftVal == "Tool") return findMenuObjCrafting(0);
+            else return findMenuObjCrafting(parseInt(craftVal));
+        },
+        events: {
+            show: function (options) {
+                menuVisible = true;
+            },
+            hide: function (options) {
+                menuVisible = false;
+            },
+        },
+        zIndex: 4,
+    });
 });
 
 function findMenuObj(x, y) {
     var i = Math.floor(x / BOXSIDE);
     var j = Math.floor(y / BOXSIDE);
-    var locWhenClicked = { i: currPlayer.i, j: currPlayer.j }
-    var structIdWhenClicked = locationMap[i][j].structure.id
+    var locWhenClicked = { i: currPlayer.i, j: currPlayer.j };
+    var structIdWhenClicked = locationMap[i][j].structure.id;
     var itemsDict = {};
     var menuDict = {};
     var locationObj = locationMap[i][j];
     itemsDict["ground"] = {
         name: `${locationObj.ground.name}`,
-        icon: "far fa-list-alt"
-    }
+        icon: "far fa-list-alt",
+    };
     itemsDict["structure"] = {
         name: `${locationObj.structure.name}`,
-        icon: "far fa-list-alt"
-    }
+        icon: "far fa-list-alt",
+    };
     for (action in locationObj.structure.actions) {
         itemsDict[action] = {
             name: `Action: ${locationObj.structure.actions[action]}`,
-            icon: 'fas fa-hammer',
+            icon: "fas fa-hammer",
             // TODO: check if player close enough to interact
             callback: function (key, opt) {
-                sendPlayerAction(structIdWhenClicked, key, getGlobalCoords({ i: i, j: j }, locWhenClicked));
-            }
-        }
+                sendPlayerAction(
+                    structIdWhenClicked,
+                    key,
+                    getGlobalCoords({ i: i, j: j }, locWhenClicked)
+                );
+            },
+        };
     }
     itemsDict["players"] = {
         name: "Players:",
         visible: function (key, opt) {
-            return (locationMap[i][j].players.length > 0);
+            return locationMap[i][j].players.length > 0;
         },
         items: (function () {
-            var players = {}
+            var players = {};
             for (index in locationMap[i][j].players) {
-                players[`${index}`] = { name: locationMap[i][j].players[index].name }
+                players[`${index}`] = {
+                    name: locationMap[i][j].players[index].name,
+                };
             }
             return players;
-        })()
-    }
+        })(),
+    };
     itemsDict["sep1"] = "---------";
     itemsDict["quit"] = {
-        name: "quit", icon: function () {
-            return 'context-menu-icon context-menu-icon-quit';
-        }
-    }
+        name: "quit",
+        icon: function () {
+            return "context-menu-icon context-menu-icon-quit";
+        },
+    };
     menuDict["items"] = itemsDict;
-    return menuDict
+    return menuDict;
 }
 
 /**
- * 
- * @param {*} invSlot slot of the inventory (starting from 0) 
+ *
+ * @param {*} invSlot slot of the inventory (starting from 0)
  */
 function findMenuObjInv(invSlot) {
     var itemsDict = {};
@@ -118,29 +149,34 @@ function findMenuObjInv(invSlot) {
         name: `${itemName}`,
         visible: function (key, opt) {
             // If item has a name
-            return Boolean(itemObj)
+            return Boolean(itemObj);
         },
-        icon: "far fa-list-alt"
-    }
+        icon: "far fa-list-alt",
+    };
     if (itemObj) {
         for (action in itemObj.actions) {
             itemsDict[action] = {
-                name: function () {
+                name: (function () {
                     if (itemObj.actions[action] == "Select") {
-                        if (currentSelectedSlot == -1 || invSlot != currentSelectedSlot) {
-                            return "Action: Select"
+                        if (
+                            currentSelectedSlot == -1 ||
+                            invSlot != currentSelectedSlot
+                        ) {
+                            return "Action: Select";
                         } else {
-                            return "Action: Deselect"
+                            return "Action: Deselect";
                         }
                     } else {
-                        return `Action: ${itemObj.actions[action]}`
+                        return `Action: ${itemObj.actions[action]}`;
                     }
-
-                }(),
-                icon: 'fas fa-hammer',
+                })(),
+                icon: "fas fa-hammer",
                 callback: function (key, opt) {
                     if (itemObj.actions[key] == "Select") {
-                        if (currentSelectedSlot == -1 || invSlot != currentSelectedSlot) {
+                        if (
+                            currentSelectedSlot == -1 ||
+                            invSlot != currentSelectedSlot
+                        ) {
                             selectInvItem(invSlot);
                             currentSelectedActionId = key;
                         } else {
@@ -152,25 +188,68 @@ function findMenuObjInv(invSlot) {
                     }
 
                     sendPlayerInvAction(itemObj.id, key, invSlot);
-                }
-            }
+                },
+            };
         }
     }
     itemsDict["sep1"] = "---------";
     itemsDict["quit"] = {
-        name: "quit", icon: function () {
-            return 'context-menu-icon context-menu-icon-quit';
-        }
-    }
+        name: "quit",
+        icon: function () {
+            return "context-menu-icon context-menu-icon-quit";
+        },
+    };
     menuDict["items"] = itemsDict;
-    return menuDict
-};
+    return menuDict;
+}
+
+/**
+ *
+ * @param {*} invSlot slot of the inventory (starting from 0)
+ */
+function findMenuObjCrafting(craftingSlot) {
+    var itemsDict = {};
+    var menuDict = {};
+    var itemObj;
+    var itemName = "";
+    console.log(craftingSlot);
+    console.log(craftingItemIdList[craftingSlot]);
+    itemObj = getItemObj(craftingItemIdList[craftingSlot]);
+    itemName = itemObj.name;
+    console.log(itemName);
+    itemsDict["itemName"] = {
+        name: `${itemName}`,
+        visible: function (key, opt) {
+            // If item has a name
+            return Boolean(itemObj);
+        },
+        icon: "far fa-list-alt",
+    };
+    if (itemObj) {
+        itemsDict["Remove"] = {
+            name: "Remove",
+            icon: "fas fa-hammer",
+            callback: function (key, opt) {
+                RemoveCraftingItem(craftingSlot)
+            },
+        };
+    }
+    itemsDict["sep1"] = "---------";
+    itemsDict["quit"] = {
+        name: "quit",
+        icon: function () {
+            return "context-menu-icon context-menu-icon-quit";
+        },
+    };
+    menuDict["items"] = itemsDict;
+    return menuDict;
+}
 
 function updateMenu() {
     $(function () {
         //$.contextMenu('update');
-        $("#overlay").contextMenu('update');
-        $("#overlay").contextMenu('hide');
+        $("#overlay").contextMenu("update");
+        $("#overlay").contextMenu("hide");
     });
 }
 
@@ -185,7 +264,7 @@ function structHasActionAtMousePos(mousePos) {
     }
     var structObj = locationMap[mousePos.i][mousePos.j].structure;
     if (structObj) {
-        var obj = structureJson.find(o => o.id == structObj.id)
+        var obj = structureJson.find((o) => o.id == structObj.id);
         if (obj) {
             // If there are actions for this structure
             if (Object.keys(obj["actions"]).length > 0) {
@@ -198,13 +277,13 @@ function structHasActionAtMousePos(mousePos) {
 
 $(function () {
     $(document).tooltip({
-        track: true
+        track: true,
     });
 });
 
 function getActionName(structId, actionId) {
     var structObj = getStructureObj({ id: structId, health: 0, owner: "game" });
     if (structObj.actions[actionId]) {
-        return structObj.actions[actionId]
+        return structObj.actions[actionId];
     }
 }
